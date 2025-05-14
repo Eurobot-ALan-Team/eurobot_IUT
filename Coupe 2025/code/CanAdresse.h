cdaa6eef3115690cd8cf8030b41a099c3afc9394
MatthiasA18 <matthias.arnould18@gmail.com>
2025-05-14 17:11:34 +0200

    addresse can

---
+++ Coupe 2025/code/CanAdresse.h
@@ -0,0 +1,78 @@
+
+const unsigned int ID_carte_COMMANDE = 0x100;
+
+
+const unsigned int ID_carte_Actionneur = 0x200;
+const unsigned int ID_carte_Actionneur_Set_Vis = 0x250;
+const unsigned int ID_carte_Actionneur_Get_Vis = 0x201;
+const unsigned int ID_carte_Actionneur_Set_Magnet = 0x251;
+const unsigned int ID_carte_Actionneur_Get_Magnet = 0x202;
+const unsigned int ID_carte_Actionneur_Set_Cerveau = 0x252;
+const unsigned int ID_carte_Actionneur_Get_Cerveau = 0x203;
+
+const unsigned int ID_carte_Actionneur_Get_Init = 0x299;
+const unsigned int ID_carte_Actionneur_Is_Init = 0x249;
+
+
+const unsigned int ID_carte_ENCODEUR = 0x300;
+const unsigned int ID_carte_ENCODEUR_distance = 0x301;
+const unsigned int ID_carte_ENCODEUR_rot_gauche = 0x302;
+const unsigned int ID_carte_ENCODEUR_rot_droite = 0x303;
+const unsigned int ID_carte_ENCODEUR_vitesse = 0x304;
+const unsigned int ID_carte_ENCODEUR_acceleration = 0x305;
+const unsigned int ID_carte_ENCODEUR_arret = 0x306;
+const unsigned int ID_carte_ENCODEUR_avance = 0x307;
+const unsigned int ID_carte_ENCODEUR_Get_Init = 0x399;
+const unsigned int ID_carte_ENCODEUR_Is_Init = 0x349;
+
+
+const unsigned int ID_carte_Lidar = 0x400;
+const unsigned int ID_carte_Lidar_Danger = 0x401;
+const unsigned int ID_carte_Lidar_Get_Status = 0x450;
+const unsigned int ID_carte_Lidar_Is_Status = 0x402;
+const unsigned int ID_carte_Lidar_Get_Data = 0x451;
+const unsigned int ID_carte_Lidar_Is_Data = 0x403;
+const unsigned int ID_carte_Lidar_Get_Init = 0x499;
+const unsigned int ID_carte_Lidar_Is_Init = 0x449;
+
+
+const unsigned int ID_carte_IHM = 0x500;
+const unsigned int ID_carte_IHM_Read_Switchs = 0x551;
+const unsigned int ID_carte_IHM_Is_Switchs = 0x501;
+const unsigned int ID_carte_IHM_Set_Led = 0x502;
+const unsigned int ID_carte_IHM_Get_Init = 0x599;
+const unsigned int ID_carte_IHM_Is_Init = 0x549;
+
+
+const unsigned int ID_carte_BAT = 0x600;
+const unsigned int ID_carte_BAT_Demande_Tension_1 = 0x650;
+const unsigned int ID_carte_BAT_Demande_Tension_2 = 0x651;
+const unsigned int ID_carte_BAT_Battterie_Faible = 0x602;
+const unsigned int ID_carte_BAT_Tension_Baterie_1 = 0x603;
+const unsigned int ID_carte_BAT_Tension_Baterie_2 = 0x604;
+const unsigned int ID_carte_BAT_Tension_5V_Puissance = 0x605;
+const unsigned int ID_carte_BAT_Tension_5V_Commande = 0x606;
+const unsigned int ID_carte_BAT_Tension_12V_Puissance = 0x607;
+const unsigned int ID_carte_BAT_Tension_48V_Puissance = 0x608;
+const unsigned int ID_carte_BAT_Set_On_5V_Puissance = 0x659;
+const unsigned int ID_carte_BAT_Is_On_5V_Puissance = 0x609;
+const unsigned int ID_carte_BAT_Set_On_12V_Puissance = 0x660;
+const unsigned int ID_carte_BAT_Is_On_12V_Puissance = 0x610;
+const unsigned int ID_carte_BAT_Set_On_48V_Puissance = 0x661;
+const unsigned int ID_carte_BAT_Is_On_48V_Puissance = 0x611;
+const unsigned int ID_carte_BAT_Set_Off_5V_Puissance = 0x662;
+const unsigned int ID_carte_BAT_Is_Off_5V_Puissance = 0x612;
+const unsigned int ID_carte_BAT_Set_Off_12V_Puissance = 0x663;
+const unsigned int ID_carte_BAT_Is_Off_12V_Puissance = 0x613;
+const unsigned int ID_carte_BAT_Set_Off_48V_Puissance = 0x664;
+const unsigned int ID_carte_BAT_Is_Off_48V_Puissance = 0x614;
+const unsigned int ID_carte_BAT_Mesure_Current_5V = 0x665;
+const unsigned int ID_carte_BAT_Is_Current_5V = 0x615;
+const unsigned int ID_carte_BAT_Mesure_Current_12V = 0x666;
+const unsigned int ID_carte_BAT_Is_Current_12V = 0x616;
+const unsigned int ID_carte_BAT_Mesure_Current_48V = 0x667;
+const unsigned int ID_carte_BAT_Is_Current_48V = 0x617;
+const unsigned int ID_carte_BAT_Get_voltage = 0x668;
+const unsigned int ID_carte_BAT_Get_init = 0x699;
+const unsigned int ID_carte_BAT_Is_init = 0x649;
+
